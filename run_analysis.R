4faaebffc5a610e8974e45f46f0823a4a6bd5135
deepikaneethirajan <deepikaneethirajan@gmail.com>
2014-11-21 15:57:35 -0600

    R script updated

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+library(plyr)
+
+setwd("/Users/deepikaneethirajan/Documents/Data Sciences Specialization/John-Hopkins/Getting & Cleaning Data/CourseProject/Trial/test")
+x_test <- read.table("X_test.txt")
+y_test <- read.table("y_test.txt")
+setwd("/Users/deepikaneethirajan/Documents/Data Sciences Specialization/John-Hopkins/Getting & Cleaning Data/CourseProject/Trial/train")
+x_train <- read.table("X_train.txt")
